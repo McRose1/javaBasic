9cec1e726d6f7717cc9358f1b0fcf67f2680e97e
Jiaye Zhu <jzhu50@ur.rochester.edu>
2020-04-13 21:58:34 -0400

    4.13

---
+++ src/Test/intToChar.java
@@ -0,0 +1,9 @@
+package Test;
+
+public class intToChar {
+    public static void main(String[] args) {
+        int num = 1;
+        char ch = (char) (num + '0');
+        System.out.println(ch);
+    }
+}
