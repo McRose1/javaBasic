23293184d7245250f3d8ad3be5e3dfb2c3601317
Jiaye Zhu <jzhu50@ur.rochester.edu>
2020-09-16 21:13:51 -0400

    9.16

---
+++ src/Lambda/Pass.java
@@ -0,0 +1,13 @@
+package Lambda;
+
+public class Pass {
+
+    public static void main(String[] args) {
+
+        greet(() -> System.out.println("Hello World!"));
+
+        public void greet()
+
+        }
+    }
+}
--- src/MathFunction/gcd_lcm.java
+++ src/MathFunction/gcd_lcm.java
@@ -1,7 +1,7 @@
 package MathFunction;
 
 /*  求两个数字的最大公约数
-    辗转相乘法（欧几里得算法）
+    辗转相除法（欧几里得算法）
 
     求两个数字的最小公倍数
     两数的乘积 / 最大公约数
@@ -19,6 +19,8 @@ public class gcd_lcm {
         System.out.println(gcd);
         System.out.println(lcm);
     }
+
+    // a 要大于 b 才能进行辗转相除法
     private static int gcd(int a, int b) {  // a = 2, b = 6
         while (b != 0) {                           // b = 6; 2; 0
             int temp = a % b;               // temp = 2 % 6 = 2; 6 % 2 = 0
